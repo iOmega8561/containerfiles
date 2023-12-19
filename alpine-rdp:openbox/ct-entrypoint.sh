329cd77821939878581569207b51e34d8753ad61
iOmega8561 <giusepperocco38@gmail.com>
2023-12-19 12:51:59 +0100

    Now xrdp executes rootless

--- alpine-rdp:openbox/ct-entrypoint.sh
+++ alpine-rdp:openbox/ct-entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-sudo xrdp-sesman
-sudo xrdp --nodaemon
+
+if [ -z "$PASSWD" ]
+then
+    echo "FATAL: No password has been set, exiting."
+    exit 1
+fi
+
+echo -e "$PASSWD\n$PASSWD" | sudo passwd default > /dev/null 2>&1
+echo "Password set to $PASSWD"
+
+sudo touch /var/log/xrdp-sesman.log
+sudo chown default:default /var/log/xrdp-sesman.log
+
+xrdp-sesman -c $HOME/.config/xrdp/sesman.ini
+exec xrdp --nodaemon -c $HOME/.config/xrdp/xrdp.ini

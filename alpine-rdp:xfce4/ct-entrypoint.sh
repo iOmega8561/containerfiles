51ebe03eaafe6202e283e9c287b9c950427c5f67
iOmega8561 <giusepperocco38@gmail.com>
2023-12-19 13:38:21 +0100

    Parametric password

--- alpine-rdp:xfce4/ct-entrypoint.sh
+++ alpine-rdp:xfce4/ct-entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+
+if [ -z "$PASSWD" ]
+then
+    echo "FATAL: No password has been set, exiting."
+    exit 1
+fi
+
+echo -e "$PASSWD\n$PASSWD" | sudo passwd default > /dev/null 2>&1
+echo "Password set to $PASSWD"
+
 sudo xrdp-sesman
-sudo xrdp --nodaemon
+exec sudo xrdp --nodaemon

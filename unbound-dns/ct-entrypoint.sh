7880352335cb3b47a0e4ca377223e248bda91d4e
iOmega8561 <giusepperocco38@gmail.com>
2023-12-20 20:01:36 +0100

    New image unbound-dns

---
+++ unbound-dns/ct-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo -e "\n"
+echo "############ Updating root hints ... #############"
+echo "##################################################"
+
+echo -e "\n"
+
+wget -S https://www.internic.net/domain/named.cache -O .config/unbound/root.hints
+
+echo -e "\n"
+echo "############# Launching unbound ... ##############"
+echo "##################################################"
+
+echo -e "\n"
+
+exec unbound -vvvdpc .config/unbound/unbound.conf

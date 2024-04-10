b1ae72c87ef71fb14bf3db7496e67b5256694c07
Giuseppe Rocco <giuseppe@MacBook-Pro-di-Giuseppe.local>
2024-04-10 20:41:34 +0200

    Root hints get updated every 86400 seconds

--- unbound-dns/ct-entrypoint.sh
+++ unbound-dns/ct-entrypoint.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 
+update_root_hints() {
+    wget -S https://www.internic.net/domain/named.cache -O .config/unbound/root.hints
+}
+
+update_every_day() {
+    while [ true ]
+    do
+        sleep 86400
+        update_root_hints
+    done
+}
+
 echo -e "\n"
 echo "############ Updating root hints ... #############"
 echo "##################################################"
-
 echo -e "\n"
 
-wget -S https://www.internic.net/domain/named.cache -O .config/unbound/root.hints
+update_root_hints
 
 echo -e "\n"
 echo "############# Launching unbound ... ##############"
 echo "##################################################"
-
 echo -e "\n"
 
+update_every_day &
 exec unbound -vvvdpc .config/unbound/unbound.conf
